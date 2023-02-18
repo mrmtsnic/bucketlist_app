985a478a188bcc4063c6499c659a119edcab9817
morimoto <mrmtsnic01@gmail.com>
2023-02-18 16:57:50 +0900

    Add Edit feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                     uniqueness: true,
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   mount_uploader :avatar, ImageUploader
   
