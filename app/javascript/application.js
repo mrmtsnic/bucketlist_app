f93cb58c3cdc9930681370811844d35cb450c75a
morimoto <mrmtsnic01@gmail.com>
2023-02-25 17:49:12 +0900

    modify

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import './custom/tooltip'
 import "popper"
 import "bootstrap"
 import '@fortawesome/fontawesome-free'
-
-// import './custom/tooltip'
